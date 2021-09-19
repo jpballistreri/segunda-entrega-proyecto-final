5bbe89b9e61bdd3dcc81bf2d1f57d0bb422f3973
Juan Pablo Ballistreri <jballistreri@buenosaires.gob.ar>
2021-09-19 13:24:42 -0300

    Se agregan filtros a persistencia en FileSystem

--- src/models/products/DAOs/fs.ts
+++ src/models/products/DAOs/fs.ts
@@ -10,13 +10,44 @@ import moment from "moment";
 export class ProductosFSDAO implements ProductBaseClass {
   private productos: ProductI[] = [];
   private nombreArchivo: string;
+  private mockData: ProductI[] = [];
 
   constructor(fileName: string) {
-    //const mockData = [
-    //  { _id: "1", nombre: "lapiz", precio: 200 },
-    //  { _id: "2", nombre: "cartuchera", precio: 250 },
-    //  { _id: "3", nombre: "boligoma", precio: 260 },
-    //];
+    this.mockData = [
+      {
+        _id: "1",
+        timestamp: "2021-09-19T12:04:09-03:00",
+        nombre: "pizza muzarella",
+        descripcion: "pizza de salsa de tomate y muzarella",
+        codigo: "muzza",
+        foto: "muzza.png",
+        precio: 600,
+        stock: 500,
+      },
+      {
+        _id: "2",
+        timestamp: "2021-09-19T12:20:09-03:00",
+        nombre: "pizza fugazzetta",
+        descripcion: "pizza de cebolla y muzarrella",
+        codigo: "fugazz",
+        foto: "fugazzetta.png",
+        precio: 700,
+        stock: 300,
+      },
+      {
+        _id: "3",
+        timestamp: "2021-09-19T12:23:09-03:00",
+        nombre: "empanada de carne",
+        descripcion: "empanada de carne picada fina",
+        codigo: "emp_carne",
+        foto: "empanadacarne.png",
+        precio: 100,
+        stock: 1000,
+      },
+    ];
+
+    this.mockData.forEach((aMock) => this.productos.push(aMock));
+
     this.nombreArchivo = fileName;
     this.leer(this.nombreArchivo);
   }
@@ -27,9 +58,12 @@ export class ProductosFSDAO implements ProductBaseClass {
       this.productos = JSON.parse(await fs.promises.readFile(archivo, "utf-8"));
     } catch (err) {
       console.log(
-        "Archivo products.json no encontrado\nCreando archivo products.json"
+        "Archivo products.json no encontrado\nCreando archivo products.json con mockData."
       );
-      await fs.promises.writeFile(this.nombreArchivo, JSON.stringify([]));
+      await fs.promises.writeFile(
+        this.nombreArchivo,
+        JSON.stringify(this.mockData, null, "\t")
+      );
       this.productos = JSON.parse(await fs.promises.readFile(archivo, "utf-8"));
     }
   }
@@ -127,12 +161,28 @@ export class ProductosFSDAO implements ProductBaseClass {
     type Conditions = (aProduct: ProductI) => boolean;
     const query: Conditions[] = [];
 
-    if (options.nombre)
-      query.push((aProduct: ProductI) => aProduct.nombre == options.nombre);
-
+    if (options.nombre) {
+      let nombre = options.nombre;
+      query.push((aProduct: ProductI) => aProduct.nombre.includes(nombre));
+    }
     if (options.precio)
       query.push((aProduct: ProductI) => aProduct.precio == options.precio);
+    if (options.precioMin) {
+      let precioMin = options.precioMin;
+      query.push((aProduct: ProductI) => aProduct.precio >= precioMin);
+    }
 
+    if (options.precioMax) {
+      let precioMax = options.precioMax;
+      query.push((aProduct: ProductI) => aProduct.precio <= precioMax);
+    }
+    if (options.stock)
+      query.push((aProduct: ProductI) => aProduct.stock == options.stock);
+
+    if (options.stockMin) {
+      let stockMin = options.stockMin;
+      query.push((aProduct: ProductI) => aProduct.stock >= stockMin);
+    }
     return this.productos.filter((aProduct) => query.every((x) => x(aProduct)));
   }
 }
