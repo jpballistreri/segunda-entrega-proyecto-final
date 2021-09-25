29ac789b708f522e9da5e05506ac43dcb2387d12
Juan Pablo Ballistreri <JBallistreri@serv.externos.prismamp.com>
2021-09-25 15:29:58 -0300

    fix newProduct

--- src/models/products/DAOs/productosMongo.ts
+++ src/models/products/DAOs/productosMongo.ts
@@ -98,7 +98,7 @@ export class ProductosAtlasDAO implements ProductBaseClass {
   async add(data: newProductI): Promise<ProductI> {
     if (!data.nombre || !data.precio) throw new Error("invalid data");
     data.timestamp = moment().format();
-    const newProduct = this.productos(data);
+    const newProduct = new this.productos(data);
     await newProduct.save();
 
     return newProduct;
